f7921f7f2deb0037a8752246da37cbcec59bf784
Chandan Gupta <chandan.gupta@factset.com>
2016-12-29 15:19:27 +0530

    Add Angular2 quickStart

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import {MenuComponent} from './menu.component';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1><my-menu></my-menu>`,
+  entryComponents: [MenuComponent]     // Directives are deprecated
 })
 export class AppComponent  { name = 'Angular'; }
